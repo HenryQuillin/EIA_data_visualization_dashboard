a5df7c1aaede77a8d7f638425ba6a30d6c233b7a
AsherFor <asherforman@stu.postoakschool.org>
2020-10-13 18:47:27 -0700

    This is Asher's Commit.

---
+++ test_file_Asher.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+data_frame = pd.DataFrame(
+    {'Countires': ['Poland', 'Mexico', 'Iceland'],
+    'Cities': ['Warsaw', 'Mexico City', 'Reykjavik'],}
+
+)
+print(data_frame)
