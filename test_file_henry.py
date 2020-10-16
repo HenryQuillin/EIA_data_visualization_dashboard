43d30d983ccf09845bee10d1096bbd5cbae02d83
HenryQuillin <henryquillin@gmail.com>
2020-10-16 09:40:16 -0500

    Added pokemon dataframe

--- test_file_henry.py
+++ test_file_henry.py
@@ -2,3 +2,15 @@ print('hello world')
 print("this is Henry's code")
 print("Change Test")
 print("Asher made a change")
+
+import pandas as pd
+
+df = pd.read_csv('pokemon_data.csv')
+
+print(df.head(3))
+
+#Read column
+print(df.columns)
+
+# Read each column
+print(df['Name'][0:4])
