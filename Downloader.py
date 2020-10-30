97048190acd1d1f1b95c2a7b30674e4c91c3cf32
AsherFor <asherforman@stu.postoakschool.org>
2020-10-29 19:06:07 -0700

    Commit

--- Downloader.py
+++ Downloader.py
@@ -1,5 +1,6 @@
 import requests as req
 import zipfile
+import pandas as pd
 
 
 web = 'https://www.eia.gov/electricity/data/eia860/xls/eia8602019.zip'
@@ -16,4 +17,9 @@ def un_zip():
     with zipfile.ZipFile('data.zip', 'r') as zip_ref:
         zip_ref.extractall(zip_file)
 # un_zip()
+def data_frame():
+    df = pd.read_excel (r'C:\Users\ramen\PycharmProjects\pythonProject\internship_repo\success_unzip\1___Utility_Y2019.xlsx')
+    print(df)
+data_frame()
+
 
