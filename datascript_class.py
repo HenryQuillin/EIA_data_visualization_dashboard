b24942aab8016f42f68f0b47d6df9395427628ca
theSummit12 <46876350+theSummit12@users.noreply.github.com>
2020-11-09 08:55:20 -0600

    Working on making a class

--- datascript_class.py
+++ datascript_class.py
@@ -1,8 +1,8 @@
 import requests as req
 from urllib.request import urlretrieve as retrieve
 import zipfile
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 import os
 import shutil
 
