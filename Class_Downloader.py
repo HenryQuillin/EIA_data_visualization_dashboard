1439eab53c0c348ff0891fcd9cf54e66bee5708c
AsherFor <asherforman@stu.postoakschool.org>
2020-11-12 17:33:08 -0800

    Commit

--- Class_Downloader.py
+++ Class_Downloader.py
@@ -5,14 +5,19 @@ import pandas as pd
 web = 'https://www.eia.gov/electricity/data/eia860/xls/eia8602019.zip'
 
 class DataGrab(object):
+
     def __init__(self, url, filename, new_file):
         self.url = url
         self.filename = filename
         self.new_file = new_file
+
+    #Method for downloading the zipfile off the website
     def download(self):
         with req.get(self.url) as rq:
             with open(self.filename, 'wb') as file:
                 file.write(rq.content)
+
+    #Method for unzipping the downloaded file
     def un_zip(self):
         with zipfile.ZipFile(self.filename, 'r') as zip_ref:
             zip_ref.extractall(self.new_file)
--- Downloader.py
+++ Downloader.py
@@ -6,17 +6,18 @@ import pandas as pd
 web = 'https://www.eia.gov/electricity/data/eia860/xls/eia8602019.zip'
 
 
-def download_file():
-    with req.get('https://www.eia.gov/electricity/data/eia860/xls/eia8602019.zip') as rq:
-        with open('data.zip', 'wb') as file:
+def download_file(url, filename):
+    with req.get(url) as rq:
+        with open(filename, 'wb') as file:
             file.write(rq.content)
-# download_file()
+download_file(web, 'general_data.zip')
 
-def un_zip():
-    zip_file = 'success_unzip'
-    with zipfile.ZipFile('data.zip', 'r') as zip_ref:
+def un_zip(new_file):
+    zip_file = new_file
+    with zipfile.ZipFile('general_data.zip', 'r') as zip_ref:
         zip_ref.extractall(zip_file)
-# un_zip()
+un_zip('general_data_folder')
+
 def data_frame():
     df = pd.read_excel (r'C:\Users\ramen\PycharmProjects\pythonProject\internship_repo\success_unzip\1___Utility_Y2019.xlsx')
     print(df)
