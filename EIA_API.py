ea08d6710857fd38a54f0dde2c95746acb2f8971
HenryQuillin <henryquillin@gmail.com>
2021-02-16 13:28:08 -0600

    Added correct data

--- EIA_API.py
+++ EIA_API.py
@@ -11,20 +11,14 @@ import plotly.express as px
 api_key = '3c109e8bc5897c4015d86e77e699ebc6'
 
 
-# PADD Names to Label Columns
-# Change to whatever column labels you want to use.
-PADD_NAMES = ['PADD 1','PADD 2','PADD 3','PADD 4','PADD 5']
+
+# column labels 
+PADD_NAMES = ['NATURAL GAS','COAL','PETROLIUM']
 # Enter all your Series IDs here separated by commas
-PADD_KEY = ['PET.MCRRIP12.M',
-'PET.MCRRIP22.M',
-'PET.MCRRIP32.M',
-'PET.MCRRIP42.M',
-'PET.MCRRIP52.M']
-# Initialize list - this is the final list that you will store all the data from the json pull. Then you will use this list to concat into a pandas dataframe. 
+PADD_KEY = ['EMISS.CO2-TOTV-TT-NG-US.A','EMISS.CO2-TOTV-TT-CO-US.A','EMISS.CO2-TOTV-TT-PE-US.A']
+#list to concat into a pandas dataframe
 final_data = []
-# Choose start and end dates
-startDate = '2009-01-01'
-endDate = '2021-01-01'
+
 
 # Pull in data via EIA API
 for i in range(len(PADD_KEY)):
@@ -44,22 +38,12 @@ for i in range(len(PADD_KEY)):
 
 
 
-
-
 # Combine all the data into one dataframe
 final_df = pd.concat(final_data, axis=1)
+final_df = final_df.sort_index(ascending=True)
 
 
-# Create date as datetype datatype
-final_df['Year'] = final_df.index.astype(str).str[:4]
-final_df['Month'] = final_df.index.astype(str).str[4:]
-final_df['Day'] = 1
-final_df['Date'] = pd.to_datetime(final_df[['Year','Month','Day']])
-final_df.set_index('Date',drop=True,inplace=True)
-final_df.sort_index(inplace=True)
-final_df = final_df[startDate:endDate]
-final_df = final_df.iloc[:,:5]
 
-
-fig = px.line(final_df, title='Net generation')
+print(final_df.head())
+fig = px.line(final_df,title='Total carbon dioxide emissions in the United States (million metric tons CO2)')
 fig.show()
